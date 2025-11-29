0b1c8e034157b87dcd77e7814a977949042980a7
cryptonal <hammad.tahir95@gmail.com>
2025-11-29 20:17:19 +0100

    feat: implement UI service for task visibility management and update header component

--- src/app/components/header.component/header.component.ts
+++ src/app/components/header.component/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Button } from "../button/button";
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -9,13 +11,17 @@ import { Button } from "../button/button";
 })
 export class HeaderComponent implements OnInit {
   title: string = 'Task Tracker';
+  showAddTask: boolean = false;
+  subscription: Subscription;
 
-  constructor() {}
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value);
+  }
 
   ngOnInit(): void {}
 
   toggleAddTask() {
-    console.log('Add button clicked');
+    this.uiService.toggleAddTask();
   }
 
 }
